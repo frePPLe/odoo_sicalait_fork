1c882f38b979f9952c20274eef615f5a105b05ee
hichamlahlou <hlahlou@frepple.com>
2025-06-30 12:01:19 +0200

    get the name of the template in French

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -1137,13 +1137,13 @@ class exporter(object):
             (
             select coalesce(product_product.default_code,
             product_template.name->>%s,
-            product_template.name->>'en_US'), count(*)
+            product_template.name->>'fr_FR'), count(*)
             from product_product
             inner join product_template on product_product.product_tmpl_id = product_template.id
             where product_template.type not in ('service', 'consu')
             group by coalesce(product_product.default_code,
             product_template.name->>%s,
-            product_template.name->>'en_US')
+            product_template.name->>'fr_FR')
             having count(*) > 1
             ) t
                 """,
